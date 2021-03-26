becfa3a8ded0dcb51a38b2b28e4a106c345279de
Oktay <alexandre35137@gmail.com>
2021-03-26 11:25:18 +0100

    black color added

--- TP2/prog.cpp
+++ TP2/prog.cpp
@@ -8,9 +8,11 @@ void test_color()
     s5loo::Color c;
     c.green_ = 165;
     s5loo::Color g = grey(red);
+    const s5loo::Color noir;
     std::cout << "Red : " << red;
     std::cout << "C : " << c;
-    std::cout << "G : " << g;
+    std::cout << "Grey : " << g;
+    std::cout << "Noir : " << noir;
 }
 
 int main(int argc, char const *argv[])
